aa27afd8c095d14ad40939d810ee6888232031ab
scotts5 <ssteinhauser@tamu.edu>
2020-10-21 14:27:48 -0500

    Added info about APIs

--- script.js
+++ script.js
@@ -75,6 +75,20 @@ function initMap() {
  *   API Calls
  * 
 *********************************/
+// Aeris Weather info (moon phase)
+const AccessID = cruQcmMBbu2IWxTzBpQxF;
+const SecretKey = ZclnNemymt1nOKIUsB37ci2U5ydvua6e6OAla33f;
+// request format: https://api.aerisapi.com//sunmoon/moonphases?limit={NUMDAYS}&client_id=cruQcmMBbu2IWxTzBpQxF&client_secret=ZclnNemymt1nOKIUsB37ci2U5ydvua6e6OAla33f
+// replace {NUMDAYS} with number of days we want moon phases (should be 7 in our case..)
+// more info: https://www.aerisweather.com/support/docs/api/reference/endpoints/sunmoon-moonphases/
+
+
+// Aeris Weather Info (weather forecast)
+// Same API ID & Key as above
+
+
+
+
 
 
 /*********************************
