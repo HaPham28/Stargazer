af93c8b5d7915b861e0fcbd23f0ee9f4341e8258
vietha-pham <vietha-pham>
2020-11-02 16:35:05 -0600

    add location mobile friendly

---
+++ src/assets/assets.js
@@ -0,0 +1,19 @@
+import central_park_img from "./central-park.jpg";
+import crescent_waning_img from "./crescent_waning.png";
+import crescent_waxing_img from "./crescent_waxing.png";
+import full_moon_img from "./full.png";
+import gibbous_waning_img from "./gibbous_waning.png";
+import gibbous_waxing_img from "./gibbous_waxing.png";
+import new_moon_img from "./new.png";
+import quarter_waning_img from "./quarter_waning.png";
+import quarter_waxing_img from "./quarter_waxing.png";
+
+export const central_park = central_park_img;
+export const crescent_waning = crescent_waning_img;
+export const crescent_waxing = crescent_waxing_img;
+export const full_moon = full_moon_img;
+export const gibbous_waning = gibbous_waning_img;
+export const gibbous_waxing = gibbous_waxing_img;
+export const new_moon = new_moon_img;
+export const quarter_waning = quarter_waning_img;
+export const quarter_waxing = quarter_waxing_img;
---
+++ webpack.config.prod.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    mode: "production",
+    entry: {
+        index: "./src/index.js",
+        astro_timeline: "./src/astroTimeline.js",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: path.resolve(__dirname, "dist", "assets"),
+    },
+    module: {
+        rules: [
+            {test: /\.css$/, use: ["style-loader", "css-loader"]},
+            {test: /\.(png|svg|jpe?g|gif)$/i, loader: 'file-loader', options: {name: "assets/[hash].[ext]"}},
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "Stargazer",
+            template: "./src/index.html",
+        }),
+    ],
+};
