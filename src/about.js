56e0b0a7a870955090ccba23dfec67719f938b09
alexlabbane <alex.labbane@tamu.edu>
2020-11-22 18:44:04 -0600

    added alex and scott about pics

--- src/about.js
+++ src/about.js
@@ -1,12 +1,12 @@
-import { central_park } from "./assets/assets";
+import { alex, scott, central_park } from "./assets/assets";
 
 window.init_about = function() {
     console.log("About Loading...");
     console.log("About Loaded");
 
-    document.getElementById("scott-pic").src = central_park;
+    document.getElementById("scott-pic").src = scott;
     document.getElementById("ha-pic").src = central_park;
-    document.getElementById("alex-pic").src = central_park;
+    document.getElementById("alex-pic").src = alex;
     document.getElementById("brett-pic").src = central_park;
     document.getElementById("vinay-pic").src = central_park;
 }
