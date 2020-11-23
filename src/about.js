e9c06d319d70a1ea81219321f2493553517b30cf
alexlabbane <alex.labbane@tamu.edu>
2020-11-22 19:00:13 -0600

    added ha to about page

--- src/about.js
+++ src/about.js
@@ -1,11 +1,11 @@
-import { alex, scott, central_park } from "./assets/assets";
+import { alex, scott, ha, central_park } from "./assets/assets";
 
 window.init_about = function() {
     console.log("About Loading...");
     console.log("About Loaded");
 
     document.getElementById("scott-pic").src = scott;
-    document.getElementById("ha-pic").src = central_park;
+    document.getElementById("ha-pic").src = ha;
     document.getElementById("alex-pic").src = alex;
     document.getElementById("brett-pic").src = central_park;
     document.getElementById("vinay-pic").src = central_park;
